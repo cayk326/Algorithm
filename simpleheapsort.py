86a88612bea526e1f3c93c6fccae2867345e3991
cayk326 <yuta0326mj@gmail.com>
2020-06-13 23:38:07 +0900

    Create heap sort using library

---
+++ simpleheapsort.py
@@ -0,0 +1,10 @@
+import heapq
+def heapsort(data):
+    h = data.copy()
+    heapq.heapify(h) # ヒープの構成
+    return [heapq.heappop(h) for _ in range(len(data))]
+
+if __name__ == '__main__':
+
+    data = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+    print(heapsort(data))
